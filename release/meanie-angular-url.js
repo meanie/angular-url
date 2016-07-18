93fef5fb42c6c9f17032c5a2997795cea96e30f0
Adam Buczynski <adambuczynski@gmail.com>
2016-07-18 20:00:42 +1200

    New release

--- release/meanie-angular-url.js
+++ release/meanie-angular-url.js
@@ -1,63 +1,58 @@
 /**
- * meanie-angular-url - v1.1.1 - 10-1-2016
+ * meanie-angular-url - v1.1.1 - 18-6-2016
  * https://github.com/meanie/angular-url
  *
  * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
  * License: MIT
  */
-(function(window, angular, undefined) {'use strict';
+'use strict';
 
-/**
- * Module definition and dependencies
- */
-angular.module('Url.Service', [])
+(function (window, angular, undefined) {
+  'use strict';
 
-/**
- * Factory definition
- */
-.factory('$url', function() {
-  return {
+  /**
+   * Module definition and dependencies
+   */
 
-    /**
-     * Url concatenation helper
-     */
-    concat: function() {
-      var parts = [];
-      for (var i = 0; i < arguments.length; i++) {
-        var part = arguments[i];
-        if (part === null || part === undefined || part === true || part === false) {
-          continue;
+  angular.module('Url.Service', [])
+
+  /**
+   * Factory definition
+   */
+  .factory('$url', function () {
+    return {
+
+      /**
+       * Url concatenation helper
+       */
+      concat: function concat() {
+        var parts = [];
+        for (var i = 0; i < arguments.length; i++) {
+          var part = arguments[i];
+          if (part === null || part === undefined || part === true || part === false) {
+            continue;
+          }
+          part = String(part);
+          if (part !== '') {
+            parts.push(part);
+          }
         }
-        part = String(part);
-        if (part !== '') {
-          parts.push(part);
-        }
+        return parts.join('/').replace(/([^:])(\/\/+)/g, '$1/').replace(/^\/\//, '/');
+      },
+
+      /**
+       * Encode URI component
+       */
+      encodeUriComponent: function encodeUriComponent(str, pctEncodeSpaces) {
+        return encodeURIComponent(str).replace(/%40/g, '@').replace(/%3A/gi, ':').replace(/%24/g, '$').replace(/%2C/gi, ',').replace(/%20/g, pctEncodeSpaces ? '%20' : '+');
+      },
+
+      /**
+       * Encode URI segments
+       */
+      encodeUriSegment: function encodeUriSegment(str) {
+        return this.encodeUriComponent(str, true).replace(/%26/gi, '&').replace(/%3D/gi, '=').replace(/%2B/gi, '+');
       }
-      return parts.join('/').replace(/([^:])(\/\/+)/g, '$1/').replace(/^\/\//, '/');
-    },
-
-    /**
-     * Encode URI component
-     */
-    encodeUriComponent: function(str, pctEncodeSpaces) {
-      return encodeURIComponent(str)
-        .replace(/%40/g, '@')
-        .replace(/%3A/gi, ':')
-        .replace(/%24/g, '$')
-        .replace(/%2C/gi, ',')
-        .replace(/%20/g, (pctEncodeSpaces ? '%20' : '+'));
-    },
-
-    /**
-     * Encode URI segments
-     */
-    encodeUriSegment: function(str) {
-      return this.encodeUriComponent(str, true).
-        replace(/%26/gi, '&').
-        replace(/%3D/gi, '=').
-        replace(/%2B/gi, '+');
-    }
-  };
-});
-
+    };
+  });
 })(window, window.angular);
