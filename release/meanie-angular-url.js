b8e834c22c781c156aef427aef37f346faba7cfe
Adam Buczynski <adambuczynski@gmail.com>
2016-07-18 20:01:00 +1200

    1.1.2

--- release/meanie-angular-url.js
+++ release/meanie-angular-url.js
@@ -1,5 +1,5 @@
 /**
- * meanie-angular-url - v1.1.1 - 18-6-2016
+ * meanie-angular-url - v1.1.2 - 18-6-2016
  * https://github.com/meanie/angular-url
  *
  * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
