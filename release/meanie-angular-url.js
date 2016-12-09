cccbf6a313645bc72e005c9d0c85ce343f61a633
Adam Reis <adambuczynski@gmail.com>
2016-12-09 21:10:35 +1300

    1.2.1

--- release/meanie-angular-url.js
+++ release/meanie-angular-url.js
@@ -1,7 +1,7 @@
 /**
  * meanie-angular-url * https://github.com/meanie/angular-url
  *
- * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
+ * Copyright (c) 2016 Adam Reis <adam@reis.nz>
  * License: MIT
  */
 (function (window, angular, undefined) {
