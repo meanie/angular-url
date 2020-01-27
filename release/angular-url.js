7d5bce8fc5117553e20976c9d6707a110124e88e
Adam Reis <adam@reis.nz>
2020-01-27 14:43:39 +1300

    2.0.2

--- release/angular-url.js
+++ release/angular-url.js
@@ -1,7 +1,7 @@
 /**
  * @meanie/angular-url * https://github.com/meanie/angular-url
  *
- * Copyright (c) 2017 Adam Reis <adam@reis.nz>
+ * Copyright (c) 2020 Adam Reis <adam@reis.nz>
  * License: MIT
  */
 (function (window, angular, undefined) {
